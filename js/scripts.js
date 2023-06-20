e803f45f2eb1e93df89a63c21536ff4c37869729
Jae Lieder <jae.lieder@gmail.com>
2023-06-20 15:02:13 -0500

    add pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,21 @@
 //pokemon array
-let pokemonList=[{},{},{}]
+let pokemonList=[
+  {
+  name: ‘Squirtle’,
+  id: 7,
+  height: 0.5,
+  type: [‘water’]
+  },
+  {
+  name: ‘Spearow’,
+  id: 21,
+  height: 0.3,
+  type: [‘flying’,‘normal’]
+  },
+  {
+  name: ‘Jigglypuff’,
+  id: 39,
+  height: 0.5,
+  type: [‘fairy’,‘normal’]
+  }
+]
