e57b22fcc3f3298d32bed1cf1cba84981337c15d
Jae Lieder <jae.lieder@gmail.com>
2023-07-09 10:50:32 -0500

    update Pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ let pokemonList=[
   {
   name: 'Squirtle',
   id: 7,
-  height: 0.5,
+  height: 5,
   type: ['water']
   },
   {
   name: 'Spearow',
   id: 21,
-  height: 0.3,
+  height: 3,
   type: ['flying','normal']
   },
   {
   name: 'Jigglypuff',
   id: 39,
-  height: 0.5,
+  height: 5,
   type: ['fairy','normal']
   }
 ];
